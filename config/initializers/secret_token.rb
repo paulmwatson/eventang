a23431e8050fe0f3e574b9d4b5bb8153e28f36f0
paulmwatson <github.com@paulmwatson.com>
2013-09-19 12:02:25 +0100

    Thunderbirds are go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eventang::Application.config.secret_key_base = '85b70a222f160b60a048c6f5c20bab9b161295e6053d899018fcabe0f2f70bbafe09f8187389b80d10c4b9c8a546efa317285d4e61cbc33d1a9da521ba3bda67'
